c18c241397f4448f946788d0b9f8809b7597b1b3
Sahara Sipple <sahara36@usf.edu>
2025-01-30 21:41:17 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,6 @@
+// Task 1: Product Price Management Scenario // 
+
+let prices = [10, 20, 30, 40, 50]; // Declares the prices array
+prices.push(60); // Adds the number 60 to the end of the array
+prices.shift(10); // Removes the number 10 from the beginning of the array
+console.log("Updated Price List:" , prices); // Displays prices array
