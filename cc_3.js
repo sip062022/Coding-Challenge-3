f1f66ef168b5ac16d2226e70166a91e6846a73c5
Sahara Sipple <sahara36@usf.edu>
2025-01-30 21:49:00 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -4,3 +4,12 @@ let prices = [10, 20, 30, 40, 50]; // Declares the prices array
 prices.push(60); // Adds the number 60 to the end of the array
 prices.shift(10); // Removes the number 10 from the beginning of the array
 console.log("Updated Price List:" , prices); // Displays prices array
+
+// Task 2: Modifying Customer Orders Scenario //
+
+let orders = [1, 2, 3, 4, 5]; // Declares the orders array
+orders.splice(2,1, 8) // Increase third order quantity of 3 by 5 to make 8
+let total = orders.reduce((sum, orders) => sum + orders, 0); // Adds quantities in the array
+
+console.log("Updated Orders Array:" , orders); // Displays the updated array
+console.log("Total Order:" , total); // Displays total sum of order quantities [20]
