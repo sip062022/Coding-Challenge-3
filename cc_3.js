c416ddcad707cbf35e4048df026954ad12bd487d
Sahara Sipple <sahara36@usf.edu>
2025-02-01 12:45:30 -0500

    Spacing Changes and formula change for task 5

--- cc_3.js
+++ cc_3.js
@@ -8,11 +8,11 @@ console.log("Updated Price List:" , prices); // Displays prices array
 // Task 2: Modifying Customer Orders Scenario //
 
 let orders = [1, 2, 3, 4, 5]; // Declares the orders array
-orders.splice(2,1, 8) // Increase third order quantity of 3 by 5 to make 8
+orders.splice(2, 1, 8) // Increase third order quantity of 3 by 5 to make 8
 let total = orders.reduce((sum, orders) => sum + orders, 0); // Adds quantities in the array
 
 console.log("Updated Orders Array:" , orders); // Displays the updated array
-console.log("Total Order:" , total); // Displays total sum of order quantities [20]
+console.log("Total Order Count:" , total); // Displays total sum of order quantities [20]
 
 // Task 3: Employee Performance Tracking Scenario //
 
@@ -28,7 +28,7 @@ employee.performanceScore = 100; // Changes perforamceScore from a 96 to a 100
 
 employee.promotionEligible = true; // Adds properly that promotionEligible is true
 
-console.log("Employee Performance Tracker:" ,employee); // Displays updated object
+console.log("Employee Performance Tracker:" , employee); // Displays updated object
 
 // Task 4: Customer Feedback Records Scenario //
 
@@ -50,9 +50,9 @@ let inventory = {  // Defines the inventory object
     stockCount: 5,  // Defines the stockCount property
     price: 7,  // Defines the price property
     calculateTotalValue: function() {  // Defines the method calcaulateTotalValue
-        console.log("Total Value:" , this.stockCount * this.price);  // Defines the formula to be used
+        return this.stockCount * this.price;  // Defines the formula to be used
     }
 };
 
 console.log("Inventory Details" , inventory); // Displays the object
-console.log(inventory.calculateTotalValue()); // Displays the method calculateTotalValue's result [35]
+console.log("Total Value:" , inventory.calculateTotalValue()); // Displays the method calculateTotalValue's result [35]
